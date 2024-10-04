dbd90c334f85fd528ed5dce34a45999b8dbfa845
Bob Jiang <jiangxb@gmail.com>
2024-10-04 17:18:32 +1000

    i

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -22,7 +22,7 @@ import { ref } from 'vue'
           <a href="https://riselabs.xyz/" target="_blank" rel="noopener noreferrer"><img src="../assets/images/Rise_Black.png" alt="Rise"></a>
         </div>
         <div class="item flex-center-center">
-          <img src="../assets/images/Hansa.jpg" alt="Hansa">
+          <a href="https://hansa.network/" target="_blank"><img src="../assets/images/Hansa.jpg" alt="Hansa"></a>
         </div>
       </div>
       <button>
