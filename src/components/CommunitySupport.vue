ceae5dd63ccd467f889e64395bbb8994538b16f7
Bob Jiang <jiangxb@gmail.com>
2024-10-12 19:14:10 +1100

    add sydweb3

--- src/components/CommunitySupport.vue
+++ src/components/CommunitySupport.vue
@@ -27,6 +27,9 @@ import { ref } from 'vue'
         <div class="item flex-center-center">
           <a href="https://www.defi.org.au/" target="_blank" rel="noopener noreferrer"><img src="../assets/images/ausdefi.png" alt="AusDefi"></a>
         </div>
+        <div class="item flex-center-center">
+          <a href="https://sydweb3.org/" target="_blank" rel="noopener noreferrer"><img src="../assets/images/sydweb3-black.jpg" alt="SydWeb3"></a>
+        </div>
       </div>
     </div>
   </div>
